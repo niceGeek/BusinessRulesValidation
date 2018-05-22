5cac2ac1527129834874175b5ba0afd92f1ba7ee
Ange Desire Shingiro <a.shingiro@itech-progress.com>
2018-05-22 15:27:18 +0200

    Projektstruktur angepasst

---
+++ src/main/java/de/shingiro/App.java
@@ -0,0 +1,16 @@
+package de.shingiro;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "DROOLS RULEZ!" );
+    }
+}
