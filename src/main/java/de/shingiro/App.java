d2edd6ae728069457898f209907ff8929a9a26e6
Ange Desire Shingiro <a.shingiro@itech-progress.com>
2018-05-29 13:28:52 +0200

    Let's see if Jenkins automatically triggers ;)

--- src/main/java/de/shingiro/App.java
+++ src/main/java/de/shingiro/App.java
@@ -15,6 +15,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "DROOLS RULEZ!" );
+        System.out.println("...just to see if Jenkins triggers ;)");
     }
 
     /*
