f7233ffce2852b23e1428a90775fec87e229b111
Ange Desire Shingiro <a.shingiro@itech-progress.com>
2018-05-29 10:08:46 +0200

    Added Jenkinsfile

--- src/main/java/de/shingiro/App.java
+++ src/main/java/de/shingiro/App.java
@@ -1,6 +1,9 @@
 package de.shingiro;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -13,4 +16,16 @@ public class App
     {
         System.out.println( "DROOLS RULEZ!" );
     }
+
+    /*
+    {
+        //Stream.of(1,2,3).map(n -> Stream.of(n, n*n)).forEach(System.out::println);
+        //System.out.println("Summe: " + Arrays.asList(1,2,3,4,5).stream().map(m -> m*m).reduce(0,  Integer::sum));
+        Long anzahl = Stream.of(1,2,3,4,5,6,7,8,9,0).collect(Collectors.counting());
+
+        System.out.println("Anzahl: " + anzahl);
+    }
+    */
+
+
 }
